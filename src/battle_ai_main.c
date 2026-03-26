7317eb4463336b5accc2b61daa71cf00a7d5cb95
Lyka <lykamaeschwarz@gmail.com>
2026-03-26 11:36:53 +0100

    workaround for partner attacking player

--- src/battle_ai_main.c
+++ src/battle_ai_main.c
@@ -943,8 +943,14 @@ static u32 ChooseMoveOrAction_Doubles(enum BattlerId battler)
             actionOrMoveIndex[battlerIndex] = mostViableMovesIndices[RandomUniform(RNG_AI_SCORE_TIE_DOUBLES_MOVE, 0, mostViableMovesNo - 1)];
             bestMovePointsForTarget[battlerIndex] = mostViableMovesScores[0];
 
+            // Steven started attacking the player, so this was changed
             // Don't use a move against ally if it has less than 100 points.
-            if (battlerIndex == BATTLE_PARTNER(battler) && bestMovePointsForTarget[battlerIndex] < AI_SCORE_DEFAULT)
+            //if (battlerIndex == BATTLE_PARTNER(battler) && bestMovePointsForTarget[battlerIndex] < AI_SCORE_DEFAULT)
+            //{
+            //    bestMovePointsForTarget[battlerIndex] = -1;
+            //}
+
+            if (battlerIndex == BATTLE_PARTNER(battler))
             {
                 bestMovePointsForTarget[battlerIndex] = -1;
             }
